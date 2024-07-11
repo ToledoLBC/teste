6a3461cc1e0b3509b18cc26831de9fd13408f433
Lumi <lucastoledobm@gmail.com>
2024-07-11 20:18:56 -0300

    teste

--- scripts/game.js
+++ scripts/game.js
@@ -8,8 +8,7 @@ botoes.innerHTML = `
         <button type="button" id="info" class="button" onclick="info()">
             <img src="/teste/midias/icon-info.svg" id="iinfo" class="icon" alt="Informação">
         </button>
-        <div class="warn" id="warn">
-        </div>
+        <div class="warn" id="warn"></div>
         <button type="button" id="maxi" class="button">
             <img src="/teste/midias/icon-maxi.svg" id="imaxi" class="icon" alt="Maximizar" onclick="maxi()">
         </button>
